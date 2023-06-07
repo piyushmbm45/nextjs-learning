92fd4c751aee4a0eb40720696d74c3792841608b
Piyush Jain <piyushmbm45@gmail.com>
2023-06-07 21:56:41 +0530

    Routing - simple routing learn

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+      <h1>About Page</h1>
     </main>
   );
 }
